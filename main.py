479b05f9d425d40cea03df653abe9fc6a1067a74
deathplow93 <rafaelalvarez93@gmail.com>
2024-08-03 22:44:12 -0500

    added read feature

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("hello world")
+file_path = "books/frankenstein.txt"
+
+def read_contents():
+    with open(file_path) as f:
+        file_contents = f.read()
+        print(file_contents)
+
+def main():
+    read_contents()
+
+if __name__ == '__main__':
+    main()
+
